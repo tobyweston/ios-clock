c3638a19b33d6825cd5e50cb5504d902088bde9a
Toby Weston <toby.weston@gmail.com>
2012-10-31 19:00:57 +0000

    adding an information view

---
+++ clock/BADInfomationViewController.h
@@ -0,0 +1,15 @@
+//
+//  BADInfomationViewController.h
+//  clock
+//
+//  Created by Toby Weston on 31/10/2012.
+//  Copyright (c) 2012 Bad Robot (London) Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BADInfomationViewController : UIViewController
+
+-(void)dismissView;
+
+@end
---
+++ clock/BADInfomationViewController.m
@@ -0,0 +1,29 @@
+//
+//  BADInfomationViewController.m
+//  clock
+//
+//  Created by Toby Weston on 31/10/2012.
+//  Copyright (c) 2012 Bad Robot (London) Ltd. All rights reserved.
+//
+
+#import "BADInfomationViewController.h"
+
+@interface BADInfomationViewController ()
+
+@end
+
+@implementation BADInfomationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
+    }
+    return self;
+}
+
+- (void)dismissView {
+     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
